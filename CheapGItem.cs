7e12181c844b81cdea0383c03ca8fa7500c6ff89
lyc-Lacewing <lycrlsu01@gmail.com>
2023-08-06 22:14:30 +0800

    cursed vs for mac

--- CheapGItem.cs
+++ CheapGItem.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ModLoader;
-using Terraria;
 using Terraria.ID;
 
 namespace CheapReforging
--- CheapReforgingConfig.cs
+++ CheapReforgingConfig.cs
@@ -1,18 +1,10 @@
-﻿using IL.Terraria.GameContent;
-using On.Terraria.GameContent;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Terraria.DataStructures;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Config;
 
 namespace CheapReforging
 {
-    [Label("$Mods.CheapReforging.Config.CRConfig.Label")]
     public class CheapReforgingConfig : ModConfig
     {
         public override ConfigScope Mode => ConfigScope.ServerSide;
@@ -26,38 +18,26 @@ namespace CheapReforging
 
         [Header(PriceKey + "Header")]
 
-        [Label(PriceKey + "UseFixed.Label")]
-        [Tooltip(PriceKey + "UseFixed.Tooltip")]
         [DefaultValue(false)]
         public bool useFixedPrice;
 
-        [Label(PriceKey + "Fixed.Label")]
-        [Tooltip(PriceKey + "Fixed.Tooltip")]
         [Range(1, int.MaxValue)]
         [DefaultValue(1)]
         public int fixedPrice;
 
-        [Label(PriceKey + "Add.Label")]
-        [Tooltip(PriceKey + "Add.Tooltip")]
         [Range(int.MinValue, int.MaxValue)]
         [DefaultValue(-50)]
         public int priceAdd;
 
-        [Label(PriceKey + "Mult.Label")]
-        [Tooltip(PriceKey + "Mult.Tooltip")]
         [Range(0, 10000f)]
         [DefaultValue(90)]
         public int priceMult;
 
         [Header(RefundKey + "Header")]
 
-        [Label(RefundKey + "UseRefund.Label")]
-        [Tooltip(RefundKey + "UseRefund.Tooltip")]
         [DefaultValue(true)]
         public bool useRefund;
 
-        [Label(RefundKey + "Mult.Label")]
-        [Tooltip(RefundKey + "Mult.Tooltip")]
         [Range(0, 10000)]
         [DefaultValue(200)]
         public int refundMult;
